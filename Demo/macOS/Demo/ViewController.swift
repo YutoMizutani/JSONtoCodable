7252f4ef9d12e55a02387eb68f44107038c560dc
YutoMizutani <yuto.mizutani.dev@gmail.com>
2018-09-22 00:49:45 +0900

    :file_folder: Create project #9

---
+++ Demo/macOS/Demo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by Yuto Mizutani on 2018/09/22.
+//  Copyright © 2018 Yuto Mizutani. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
