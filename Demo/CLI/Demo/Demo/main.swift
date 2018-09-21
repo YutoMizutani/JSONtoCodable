a3892ff6bc8af6a8d3eeb49b21af0f6da1644381
YutoMizutani <yuto.mizutani.dev@gmail.com>
2018-09-21 21:28:27 +0900

    :file_folder: Create project

---
+++ Demo/CLI/Demo/Demo/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Demo
+//
+//  Created by Yuto Mizutani on 2018/09/21.
+//  Copyright © 2018 Yuto Mizutani. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
