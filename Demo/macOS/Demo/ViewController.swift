d5660d1bd613c40fe31e18a839cec1a18773622d
YutoMizutani <yuto.mizutani.dev@gmail.com>
2018-09-22 11:00:25 +0900

    :tada: Add macOS demo! #9

--- Demo/macOS/Demo/ViewController.swift
+++ Demo/macOS/Demo/ViewController.swift
@@ -7,21 +7,59 @@
 //
 
 import Cocoa
+import JSONtoCodable
 
 class ViewController: NSViewController {
+    @IBOutlet weak var textView: NSTextView!
+    @IBOutlet weak var translateButton: NSButton!
+
+    var codable: JSONtoCodable = JSONtoCodable()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.configureSetting()
+        self.configureView()
+        self.configureAction()
+    }
+}
+
+// MARK: - configure view
+
+private extension ViewController {
+    private func configureSetting() {
+        self.codable.config.accessModifer = .public
     }
 
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
+    private func configureView() {
+        self.textView.string = """
+        {
+            "user": {
+                "Name": "Yuto Mizutani"
+            }
+            "lib": {
+                "lib-name": "JSONtoCodable"
+                "year": 2018
+                "version": "1.0.2"
+                "released": "2018-09-22"
+            }
+            "text": "Hello, world!!"
         }
+        """
     }
 
+    private func configureAction() {
+        self.translateButton.action = #selector(self.translate)
+    }
+}
 
+// MARK: - actions
+
+private extension ViewController {
+    @objc private func translate() {
+        print(self.textView.string)
+        guard let text = try? self.codable.translate(self.textView.string) else { return }
+        self.textView.string = text
+    }
 }
 
