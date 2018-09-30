6517f84e1930e2b62c22e0eb352ed800d0528f5d
YutoMizutani <yuto.mizutani.dev@gmail.com>
2018-09-30 14:01:34 +0900

    :arrow_up: Update Xcode 10 and Swift 4.2

--- Tests/JSONtoCodableTests/JSONtoCodableTests.swift
+++ Tests/JSONtoCodableTests/JSONtoCodableTests.swift
@@ -204,7 +204,7 @@ class JSONtoCodableTests: XCTestCase {
         internalStructString = "struct Single2: Codable {\n    let double1: String\n\n    private enum CodingKeys: String, CodingKey {\n        case double1 = \"Double1\"\n    }\n}"
         property = Property(structTitle)
         property.structs = [internalStructString]
-        property.immutables = values.map { (key: $0, "", type: $0 != "Single2" ? .string : .struct("Single2")) }.map { self.base.createImmutable($0)! }
+        property.immutables = values.map { (key: $0, "", type: $0 != "Single2" ? .string : .struct("Single2")) as JSONtoCodable.RawJSON }.map { self.base.createImmutable($0)! }
         property.codingKeys = values.map { self.base.createCodingKey($0) }
         expectation = """
         struct Result: Codable {
@@ -234,7 +234,7 @@ class JSONtoCodableTests: XCTestCase {
         internalStructString = "struct Single2: Codable {\n    let double1: String\n    let double2: Double2\n    let double3: String\n\n    struct Double2: Codable {\n        let triple1: String\n\n        private enum CodingKeys: String, CodingKey {\n            case triple1 = \"Triple1\"\n        }\n    }\n\n    private enum CodingKeys: String, CodingKey {\n        case double1 = \"Double1\"\n        case double2 = \"Double2\"\n        case double3 = \"Double3\"\n    }\n}"
         property = Property(structTitle)
         property.structs = [internalStructString]
-        property.immutables = values.map { (key: $0, "", type: $0 != "Single2" ? .string : .struct("Single2")) }.map { self.base.createImmutable($0)! }
+        property.immutables = values.map { (key: $0, "", type: $0 != "Single2" ? .string : .struct("Single2")) as JSONtoCodable.RawJSON }.map { self.base.createImmutable($0)! }
         property.codingKeys = values.map { self.base.createCodingKey($0) }
         expectation = """
         struct Result: Codable {
