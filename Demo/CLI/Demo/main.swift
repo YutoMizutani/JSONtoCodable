f6d4a33c17906740d1c5e874c7e62ad7ae41f172
YutoMizutani <yuto.mizutani.dev@gmail.com>
2018-10-22 18:00:17 +0900

    :tada: Add argument value support on CLI #93

--- Demo/CLI/Demo/main.swift
+++ Demo/CLI/Demo/main.swift
@@ -14,9 +14,39 @@ let args = ProcessInfo.processInfo.arguments
 for (i, e) in args.enumerated() {
     switch e {
     // Struct name
-    case "-name", "-n":
+    case "-n", "--name":
         if i + 1 < args.count {
             codable.config.name = args[i + 1]
+        }
+    // AccessModifier
+    case "-a", "--access-modifier":
+        if i + 1 < args.count,
+            let accessModifier = AccessModifier(args[i + 1]) {
+            codable.config.accessModifier = accessModifier
+        }
+    // caseType: variable
+    case "-cv", "--case-variable":
+        if i + 1 < args.count,
+            let caseVariable = CaseType(args[i + 1]) {
+            codable.config.caseType.variable = caseVariable
+        }
+    // CaseType: struct
+    case "-cs", "--case-struct":
+        if i + 1 < args.count,
+            let caseStruct = CaseType(args[i + 1]) {
+                codable.config.caseType.struct = caseStruct
+        }
+    // LineType
+    case "-l", "--line-type":
+        if i + 1 < args.count,
+            let lineType = LineType(args[i + 1]) {
+            codable.config.lineType = lineType
+        }
+    // IndentType
+    case "-i", "--indent-type":
+        if i + 1 < args.count,
+            let indentType = IndentType(args[i + 1]) {
+            codable.config.indentType = indentType
         }
     default:
         break
