45bc983d91ae655bd88dd5dc07394ce51c94a7b2
YutoMizutani <yuto.mizutani.dev@gmail.com>
2018-10-23 13:12:34 +0900

    :tada: Add sources #91

---
+++ Demo/CLI/jc/Sources/jc/Extensions.swift
@@ -0,0 +1,126 @@
+//
+//  Extensions.swift
+//  jc
+//
+//  Created by Yuto Mizutani on 2018/10/22.
+//  Copyright © 2018 Yuto Mizutani. All rights reserved.
+//
+
+import JSONtoCodable
+
+extension AccessModifier {
+    init?(_ text: String) {
+        switch text {
+        case "d", "default":
+            self = .default
+        case "pr", "private":
+            self = .private
+        case "f", "fileprivate":
+            self = .fileprivate
+        case "i", "internal":
+            self = .internal
+        case "pu", "public":
+            self = .public
+        case "o", "open":
+            self = .open
+        default:
+            return nil
+        }
+    }
+
+    var parameterString: String {
+        switch self {
+        case .default:
+            return "default"
+        case .private:
+            return "private"
+        case .fileprivate:
+            return "fileprivate"
+        case .internal:
+            return "internal"
+        case .public:
+            return "public"
+        case .open:
+            return "open"
+        }
+    }
+}
+
+extension CaseType {
+    init?(_ text: String) {
+        switch text {
+        case "p", "pascal":
+            self = .pascal
+        case "c", "camel":
+            self = .camel
+        case "sn", "snake":
+            self = .snake
+        case "ss", "screaming-snake":
+            self = .screamingSnake
+        default:
+            return nil
+        }
+    }
+
+    var parameterString: String {
+        switch self {
+        case .pascal:
+            return "PascalCase"
+        case .camel:
+            return "camelCase"
+        case .snake:
+            return "snake_case"
+        case .screamingSnake:
+            return "SCREAMING_SNAKE_CASE"
+        }
+    }
+}
+
+extension LineType {
+    init?(_ text: String) {
+        switch text {
+        case "\n", "\\n", "n", "line-feed":
+            self = .lineFeed
+        case "\r", "\\r", "r", "carriage-return":
+            self = .carriageReturn
+        case "\r\n", "\\r\\n", "rn", "both":
+            self = .both
+        default:
+            return nil
+        }
+    }
+
+    var parameterString: String {
+        switch self {
+        case .lineFeed:
+            return "Line Feed (LF)"
+        case .carriageReturn:
+            return "Carriage Return (CR)"
+        case .both:
+            return "Both (CRLF)"
+        }
+    }
+}
+
+extension IndentType {
+    init?(_ text: String) {
+        guard let num = Int(String(text[text.index(after: text.startIndex)..<text.endIndex])) else { return nil }
+
+        if text.hasPrefix("s") {
+            self = .space(num)
+        } else if text.hasPrefix("t") {
+            self = .tab(num)
+        }
+
+        return nil
+    }
+
+    var parameterString: String {
+        switch self {
+        case .space(let n):
+            return "\(n) spaces"
+        case .tab(let n):
+            return "\(n) tabs"
+        }
+    }
+}
--- Demo/CLI/jc/Sources/jc/main.swift
+++ Demo/CLI/jc/Sources/jc/main.swift
@@ -1,1 +1,82 @@
-print("Hello, world!")
+//
+//  main.swift
+//  jc
+//
+//  Created by Yuto Mizutani on 2018/09/21.
+//  Copyright © 2018 Yuto Mizutani. All rights reserved.
+//
+
+import Foundation
+import JSONtoCodable
+
+let codable = JSONtoCodable()
+let argv = ProcessInfo.processInfo.arguments
+
+let args = Array(argv[1..<argv.count])
+for (i, e) in args.enumerated() {
+    switch e {
+
+    case "-h", "help":
+        if i + 1 < args.count {
+            helpCommand(args[i + 1])
+        } else {
+            help()
+        }
+        exit(EXIT_SUCCESS)
+
+    // Struct name
+    case "-n", "--name":
+        if i + 1 < args.count {
+            codable.config.name = args[i + 1]
+        }
+    // AccessModifier
+    case "-a", "--access-modifier":
+        if i + 1 < args.count,
+            let accessModifier = AccessModifier(args[i + 1]) {
+            codable.config.accessModifier = accessModifier
+        }
+    // caseType: variable
+    case "-cv", "--case-variable":
+        if i + 1 < args.count,
+            let caseVariable = CaseType(args[i + 1]) {
+            codable.config.caseType.variable = caseVariable
+        }
+    // CaseType: struct
+    case "-cs", "--case-struct":
+        if i + 1 < args.count,
+            let caseStruct = CaseType(args[i + 1]) {
+                codable.config.caseType.struct = caseStruct
+        }
+    // LineType
+    case "-l", "--line-type":
+        if i + 1 < args.count,
+            let lineType = LineType(args[i + 1]) {
+            codable.config.lineType = lineType
+        }
+    // IndentType
+    case "-i", "--indent-type":
+        if i + 1 < args.count,
+            let indentType = IndentType(args[i + 1]) {
+            codable.config.indentType = indentType
+        }
+
+    default:
+        print("Error: Unknown command: \(e)")
+        exit(EXIT_FAILURE)
+        break
+    }
+}
+
+if let input = String(data: FileHandle.standardInput.availableData, encoding: .utf8) {
+    do {
+        let result = try codable.generate(input)
+        print(result)
+        exit(EXIT_SUCCESS)
+    } catch JSONError.wrongFormat {
+        print("Wrong JSON format!!")
+        exit(EXIT_FAILURE)
+    } catch let e {
+        print(e.localizedDescription)
+        exit(EXIT_FAILURE)
+    }
+}
