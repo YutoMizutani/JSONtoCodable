9faa5e6d637f328b8990b3d67569f260eff06364
YutoMizutani <yuto.mizutani.dev@gmail.com>
2018-10-25 15:05:32 +0900

    :tada: Add contents #118

--- Demo/JaCo/JaCo/ViewController.swift
+++ Demo/JaCo/JaCo/ViewController.swift
@@ -2,26 +2,62 @@
 //  ViewController.swift
 //  JaCo
 //
-//  Created by Yuto Mizutani on 2018/10/25.
+//  Created by Yuto Mizutani on 2018/09/22.
 //  Copyright © 2018 Yuto Mizutani. All rights reserved.
 //
 
 import Cocoa
+import JSONtoCodable
 
 class ViewController: NSViewController {
+    @IBOutlet weak var textView: NSTextView!
+    @IBOutlet weak var generateButton: NSButton!
+
+    var codable: JSONtoCodable = JSONtoCodable()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.configureSetting()
+        self.configureView()
+        self.configureAction()
     }
-
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
-        }
-    }
-
-
 }
 
+// MARK: - configure view
+
+private extension ViewController {
+    private func configureSetting() {
+        self.codable.config.accessModifier = .public
+    }
+
+    private func configureView() {
+        self.textView.string = """
+        {
+            "user": {
+                "Name": "Yuto Mizutani"
+            },
+            "lib": {
+                "lib-name": "JSONtoCodable",
+                "year": 2018,
+                "version": "2.1.1",
+                "released": "2018-09-22"
+            },
+            "text": "Hello, world!!"
+        }
+        """
+    }
+
+    private func configureAction() {
+        self.generateButton.action = #selector(self.generate)
+    }
+}
+
+// MARK: - actions
+
+private extension ViewController {
+    @objc private func generate() {
+        guard let text = try? self.codable.generate(self.textView.string) else { return }
+        self.textView.string = text
+    }
+}
