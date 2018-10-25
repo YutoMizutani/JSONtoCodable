fa28decaefbddf83c08f51062676ca505cccac10
YutoMizutani <yuto.mizutani.dev@gmail.com>
2018-10-25 14:58:53 +0900

    :sparkles: Create project #118

---
+++ Demo/JaCo/JaCo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  JaCo
+//
+//  Created by Yuto Mizutani on 2018/10/25.
+//  Copyright © 2018 Yuto Mizutani. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Demo/JaCo/JaCo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  JaCo
+//
+//  Created by Yuto Mizutani on 2018/10/25.
+//  Copyright © 2018 Yuto Mizutani. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
